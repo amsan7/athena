579617b4a087406bff25546518fdd35ba9c613fa
Alessandro <alessandro@ubuntu.(none)>
2015-01-28 08:00:07 -0800

    created answer view, modified detail template

--- athena/forum/urls.py
+++ athena/forum/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>\d+)/answer/$', views.answer, name='answer'),
 )
--- athena/forum/views.py
+++ athena/forum/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from forum.models import Question, Answer
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 
 def index(request):
@@ -18,4 +19,20 @@ def detail(request, question_id):
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
 	return HttpResponse(respone % question_id)
+
+def answer(request, question_id):
+        q = get_object_or_404(Question, pk=question_id)
+        try:
+                original_question=Question.objects.get(pk=question_id)
+                answer=Answer(question=original_question, answer_text=request.POST['answer'])
+        except (KeyError, Answer.DoesNotExist):
+                #redisplay question form
+                return render(request, 'forum/detail', {
+                        'question': q,
+                        'error_message': "Did not submit an answer!",
+                })
+        else:
+                answer.save()
+                #note: always return redirect after dealing with POST data
+                return HttpResponseRedirect(reverse('forum:detail', args=(q.id,)))
 
